56220a5d991e9609d87804e50224c37ee585d5c3
Jean <rjeangonzalo@gmail.com>
2019-11-13 11:45:50 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Header from './components/layout/Header';
+import User from './components/User';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CssBaseline />
+      <Header>
+        <User />
+      </Header>
+      <Routes />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Chat/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Chat = () => {
+    return (
+        <h3>Soy chat.js   </h3>
+    );
+};
+
+
+export default Chat;
